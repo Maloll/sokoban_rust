abecd4cb53a56092375e089e49dfd50ae6632fcf
Maloll <malo.ollivier@etudiant.univ-rennes.fr>
2026-04-26 21:42:13 +0200

    ajout cibles et victoire

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ type CharTab = Vec<Vec<char>>;
 const SOK: char = '💂';
 const CAISSE: char = '📦';
 const MUR: char = '⬛';
+const CIBLE: char = '🔸';
+const VIDE: char = ' ';
 
 struct Pos {
     x: i32,
@@ -21,7 +23,8 @@ struct Pos {
 
 struct Game {
     map: CharTab,
-    sokPos: Pos,
+    sok_pos: Pos,
+    pos_cibles: Vec<Pos>,
     victoire: bool,
 }
 
@@ -33,6 +36,7 @@ fn main() {
     let mut jeu = Game::init();
 
     execute!(out, cursor::MoveTo(0, 0)).unwrap();
+
     jeu.show();
 
     loop {
@@ -44,6 +48,12 @@ fn main() {
                 'q' => jeu.MoveSoko(-1, 0),
                 'x' => break,
                 _ => (),
+            }
+
+            cibles(&jeu);
+            if victory(&jeu) {
+                println!("VICTOIRE !");
+                break;
             }
         }
     }
@@ -73,13 +83,16 @@ fn key_pressed() -> Result<char, bool> {
 
 impl Game {
     fn init() -> Self {
-        let mut map_init = vec![vec![' '; 15]; 15];
+        let mut map_init = vec![vec![VIDE; 15]; 15];
         let pos_init = Pos { x: 1, y: 1 };
         let pos_caisses = vec![
-            Pos { x: 1, y: 2 },
+            Pos { x: 4, y: 2 },
             Pos { x: 10, y: 10 },
-            Pos { x: 6, y: 13 },
+            Pos { x: 6, y: 12 },
         ];
+
+        let position_cibles: Vec<Pos> =
+            vec![Pos { x: 4, y: 5 }, Pos { x: 7, y: 7 }, Pos { x: 13, y: 6 }];
 
         for i in 0..15 {
             for j in 0..15 {
@@ -89,14 +102,19 @@ impl Game {
             }
         }
 
-        map_init[pos_init.y as usize][pos_init.x as usize] = SOK;
-        for caisse in pos_caisses {
+        for caisse in &pos_caisses {
             map_init[caisse.y as usize][caisse.x as usize] = CAISSE;
+        }
+
+        map_init[pos_init.y as usize][pos_init.x as usize] = SOK;
+        for cible in &position_cibles {
+            map_init[cible.y as usize][cible.x as usize] = CIBLE;
         }
 
         Self {
             map: map_init,
-            sokPos: pos_init,
+            sok_pos: pos_init,
+            pos_cibles: position_cibles,
             victoire: false,
         }
     }
@@ -104,7 +122,7 @@ impl Game {
     fn show(&self) {
         for ligne in &self.map {
             for case in ligne {
-                if *case == ' ' {
+                if *case == VIDE {
                     print!("  ");
                 } else {
                     print!("{}", case);
@@ -116,8 +134,8 @@ impl Game {
     }
 
     fn MoveSoko(&mut self, dep_x: i32, dep_y: i32) {
-        let old_x = self.sokPos.x;
-        let old_y = self.sokPos.y;
+        let old_x = self.sok_pos.x;
+        let old_y = self.sok_pos.y;
 
         let x = old_x + dep_x;
         let y = old_y + dep_y;
@@ -149,24 +167,42 @@ impl Game {
             draw_at((x_caisse * 2) as u16, y_caisse as u16, CAISSE);
         }
 
-        self.map[old_y as usize][old_x as usize] = ' ';
+        self.map[old_y as usize][old_x as usize] = VIDE;
         self.map[y as usize][x as usize] = SOK;
 
-        self.sokPos.x = x;
-        self.sokPos.y = y;
+        self.sok_pos.x = x;
+        self.sok_pos.y = y;
 
-        draw_at((old_x * 2) as u16, old_y as u16, ' ');
+        draw_at((old_x * 2) as u16, old_y as u16, VIDE);
         draw_at((x * 2) as u16, y as u16, SOK);
     }
 }
 
 fn draw_at(x: u16, y: u16, c: char) {
     let mut out = stdout();
-    if c == ' ' {
+    if c == VIDE {
         execute!(out, cursor::MoveTo(x, y), Print("  ")).unwrap();
     } else {
         execute!(out, cursor::MoveTo(x, y), Print(c)).unwrap();
     }
     let _ = out.flush();
     execute!(stdout(), cursor::MoveTo(40, 40)).unwrap();
+}
+
+fn cibles(jeu: &Game) {
+    for pos in &jeu.pos_cibles {
+        if jeu.map[pos.y as usize][pos.x as usize] == VIDE {
+            draw_at((pos.x * 2) as u16, pos.y as u16, CIBLE);
+        }
+    }
+}
+
+fn victory(jeu: &Game) -> bool {
+    for pos in &jeu.pos_cibles {
+        if jeu.map[pos.y as usize][pos.x as usize] != CAISSE {
+            return false;
+        }
+    }
+
+    return true;
 }
